b61f61126ce46ffc8c72e2aad8452c2746e99aac
dovhopolyi-do/56-rEb_G3Kh-Nm90 <dovhopolyi_do@groupbwt.com>
2022-10-07 15:10:39 +0300

    Stylization list with conferences

--- src/components/Conference.js
+++ src/components/Conference.js
@@ -5,35 +5,33 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function Conference({ title, date }) {
   return (
-    <div className="list-group meet m-2 d-flex">
-      <div class="list-group-item list-group-item-action flex-column align-items-start w-80">
-        <div class="d-flex w-100 justify-content-between ">
-          <h5 class="mb-1">{title}</h5>
-          <small>{date}</small>
-        </div>
-        <div className="meet__functionality d-flex justify-content-between m-3">
-          <Link type="button" className="btn btn-info w-25">
-            Info
-          </Link>
-          <button
-            className="meet__btn btn btn-danger w-25 "
-            style={{ opacity: 0.95 }}
-          >
-            Delete
-          </button>
-          <Link type="button" class="btn btn-warning w-25">
-            Edit
-          </Link>
-        </div>
-        <div
-          className="share-conf"
-          style={{ display: 'flex', justifyContent: 'space-between' }}
+    <div className="p-2">
+      <div class="d-flex w-100 justify-content-between m-1">
+        <h5 class="mb-1">{title}</h5>
+        <small>{date}</small>
+      </div>
+      <div className="meet__functionality d-flex justify-content-between m-3">
+        <Link type="button" className="btn btn-info w-25">
+          Info
+        </Link>
+        <button
+          className="meet__btn btn btn-danger w-25 "
+          style={{ opacity: 0.95 }}
         >
-          <button className="btn btn-success">Join</button>
-          <div className="social">
-            <FacebookIcon />
-            <TwitterIcon />
-          </div>
+          Delete
+        </button>
+        <Link type="button" class="btn btn-warning w-25">
+          Edit
+        </Link>
+      </div>
+      <div
+        className="share-conf"
+        style={{ display: 'flex', justifyContent: 'space-between' }}
+      >
+        <button className="btn btn-success">Join</button>
+        <div className="social">
+          <FacebookIcon />
+          <TwitterIcon />
         </div>
       </div>
     </div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,20 +2,36 @@ import React from 'react';
 import Header from '../components/Header';
 import { conferences } from '../mock/conference';
 import Conference from '../components/Conference';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 export default function Home() {
   return (
     <>
       <Header />
-      <div>Your Conferences</div>
       <div className="conferences">
         <p>All Conferences</p>
-        {conferences.map((conference) => (
-          <Conference
-            key={conference.id}
-            title={conference.title}
-            date={conference.date}
-          />
-        ))}
+        <Box sx={{ width: '100%' }}>
+          <Stack spacing={2} sx={{ margin: '1%' }}>
+            {conferences.map((conference) => (
+              <Item>
+                <Conference
+                  key={conference.id}
+                  title={conference.title}
+                  date={conference.date}
+                />
+              </Item>
+            ))}
+          </Stack>
+        </Box>
       </div>
     </>
   );
